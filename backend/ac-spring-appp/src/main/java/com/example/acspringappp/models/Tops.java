ed61b233db415bea6e81c2baf696fc0d45887f9e
Omari Brown <omaris.brown@gmail.com>
2020-08-28 15:46:38 -0400

    finished tops model

--- backend/ac-spring-appp/src/main/java/com/example/acspringappp/models/Tops.java
+++ backend/ac-spring-appp/src/main/java/com/example/acspringappp/models/Tops.java
@@ -16,6 +16,53 @@ public class Tops {
     private String itemName;
 
     @Column
-    private String 
+    private String price;
 
+    @Column
+    private String size;
+
+    @Column
+    private String condition;
+
+    public Tops() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
 }
