162c50d5c47b8a9d6645b8119d0b1c384a33dd52
Omari Brown <omaris.brown@gmail.com>
2020-09-01 10:27:08 -0400

    added shoes service impl

---
+++ backend/ac-spring-appp/src/main/java/com/example/acspringappp/service/ShoesEtcServiceImpl.java
@@ -0,0 +1,33 @@
+package com.example.acspringappp.service;
+
+
+import com.example.acspringappp.models.ShoesEtc;
+import com.example.acspringappp.repositories.ShoesEtcRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
+public class ShoesEtcServiceImpl implements ShoesEtcService {
+    @Autowired
+    private ShoesEtcRepository shoesEtcRepository;
+
+    @Override
+    public Iterable<ShoesEtc> listShoesEtc() {
+        return shoesEtcRepository.findAll();
+    }
+
+    @Override
+    public ShoesEtc createShoesEtc(ShoesEtc shoesEtc) {
+        return shoesEtcRepository.save(shoesEtc);
+    }
+
+    @Override
+    public ShoesEtc seeShoesEtc(String itemName) {
+        return shoesEtcRepository.seeShoesEtc(itemName);
+    }
+
+    @Override
+    public HttpStatus deleteShoesEtc(Long itemId) {
+        shoesEtcRepository.deleteById(itemId);
+        return HttpStatus.OK;
+    }
+}
