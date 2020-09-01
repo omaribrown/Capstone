d6ace8bba33b09745b2258165598e663066c84f4
Omari Brown <omaris.brown@gmail.com>
2020-09-01 10:19:23 -0400

    Added shoes model

---
+++ backend/ac-spring-appp/src/main/java/com/example/acspringappp/models/ShoesEtc.java
@@ -0,0 +1,68 @@
+package com.example.acspringappp.models;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "shoes_etc")
+public class ShoesEtc {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String itemName;
+
+    @Column
+    private String price;
+
+    @Column
+    private String size;
+
+    @Column
+    private String condition;
+
+    public ShoesEtc() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+}
